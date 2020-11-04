03c3f1c14046df2bec5c99a7905210f3874b3297
buxtonwat3r <500481@chrome.escg.ac.uk>
2020-11-04 13:59:08 +0000

    no message

---
+++ escaperoom1/Assets/scripts/cameras.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cameras : MonoBehaviour
+{
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+    public Text cam1, cam2;
+    void Start()
+    {
+        keys.Add("cam1", KeyCode.C);
+        keys.Add("cam2", KeyCode.V);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
